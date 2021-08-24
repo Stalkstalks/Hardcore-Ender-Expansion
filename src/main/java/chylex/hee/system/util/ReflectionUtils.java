d583927c4acb534b05a6f798d3c9fc38ef242eca
Alkalus <3060479+draknyte1@users.noreply.github.com>
2021-08-24 20:27:01 +0100

    Forgot about obfuscation.

--- src/main/java/chylex/hee/system/util/ReflectionUtils.java
+++ src/main/java/chylex/hee/system/util/ReflectionUtils.java
@@ -1,14 +1,77 @@
 package chylex.hee.system.util;
 
+import java.io.IOException;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+import cpw.mods.fml.relauncher.CoreModManager;
+import cpw.mods.fml.relauncher.ReflectionHelper;
+import net.minecraft.launchwrapper.Launch;
+
 public class ReflectionUtils {
 
+	public static Boolean obf;
 	public static Map<String, CachedField> mCachedFields = new LinkedHashMap<String, CachedField>();
+	
+	private static HashMap<String, String> obfFieldNames = new HashMap<String, String>();
+	
+	static {
+		obfFieldNames.put("registryObjects", "field_82596_a");
+		obfFieldNames.put("underlyingIntegerMap", "field_148759_a");
+		obfFieldNames.put("block", "field_150939_a");
+		obfFieldNames.put("lastDamage", "field_110153_bc");
+		obfFieldNames.put("equipmentDropChances", "field_82174_bp");
+		obfFieldNames.put("fire", "field_70151_c");
+		obfFieldNames.put("targetedEntity", "field_70792_g");
+		obfFieldNames.put("tagList", "field_74747_a");
+		obfFieldNames.put("floatingTickCount", "field_147365_f");
+		obfFieldNames.put("lastPosX", "field_147373_o");
+		obfFieldNames.put("lastPosY", "field_147382_p");
+		obfFieldNames.put("lastPosZ", "field_147381_q");
+		obfFieldNames.put("endRNG", "field_73204_i");
+		obfFieldNames.put("sky", "field_76779_k");
+		obfFieldNames.put("chunkProvider", "field_73020_y");
+		obfFieldNames.put("mcMusicTicker", "field_147126_aw");
+		obfFieldNames.put("xSize", "field_146999_f");
+		obfFieldNames.put("ySize", "field_147000_g");
+		obfFieldNames.put("mapSoundPositions", "field_147593_P");
+	}
 
+	public static boolean checkObfuscated() {
+		if (obf != null) {
+			return obf;
+		}		
+		boolean obfuscated = false;
+		try {
+			obfuscated = !(boolean) ReflectionHelper.findField(CoreModManager.class, "deobfuscatedEnvironment").get(null);
+		} catch (IllegalArgumentException | IllegalAccessException e) {
+			e.printStackTrace();
+			byte[] bs;
+			try {
+				bs = Launch.classLoader.getClassBytes("net.minecraft.world.World");
+				if (bs != null) {
+					obfuscated = false;
+				} else {
+					obfuscated = true;
+				}
+			} catch (IOException e1) {
+				e1.printStackTrace();
+				obfuscated = false;
+			}
+		}
+		return obfuscated;
+	}	
+	
+	public static String getCorrectFieldName(String aFieldName) {
+		if (checkObfuscated()) {
+			aFieldName = obfFieldNames.get(aFieldName);
+		}
+		return aFieldName;
+	}
+	
 
 	private static class CachedField {
 		private final Field FIELD;
@@ -40,10 +103,11 @@ public class ReflectionUtils {
 	 * @param aFieldName - Field name in {@link String} form.
 	 * @return - Valid, non-final, {@link Field} object, or {@link null}.
 	 */
-	public static Field getField(final Class<?> aClass, final String aFieldName) {
+	public static Field getField(final Class<?> aClass, String aFieldName) {
 		if (aClass == null || aFieldName == null || aFieldName.length() <= 0) {
 			return null;
 		}		
+		aFieldName = getCorrectFieldName(aFieldName);
 		CachedField y = mCachedFields.get(aClass.getName()+"."+aFieldName);
 		if (y == null) {
 			Field u;
@@ -68,13 +132,14 @@ public class ReflectionUtils {
 	 * @param aFieldName - Field name in {@link String} form.
 	 * @return - Valid, non-final, {@link Field} object, or {@link null}.
 	 */
-	public static Field getField(final Object aInstance, final String aFieldName) {
+	public static Field getField(final Object aInstance, String aFieldName) {
 		return getField(aInstance.getClass(), aFieldName);
 	}
 
 
 	@SuppressWarnings("unchecked")
-	public static <T> T getFieldValue(final Object aInstance, final String aFieldName) {
+	public static <T> T getFieldValue(final Object aInstance, String aFieldName) {		
+		aFieldName = getCorrectFieldName(aFieldName);
 		try {			
 			return (T) getField(aInstance, aFieldName).get(aInstance);
 		} catch (IllegalArgumentException | IllegalAccessException e) {
@@ -82,12 +147,13 @@ public class ReflectionUtils {
 		}
 	}
 
-	public static void setFieldValue(final Object aInstance, final String aFieldName, Object aNewValue) {
+	public static void setFieldValue(final Object aInstance, String aFieldName, Object aNewValue) {
 		setFieldValue(aInstance, aFieldName, aInstance instanceof Class ? true : false, aNewValue);
 	}
 
-	public static void setFieldValue(final Object aInstance, final String aFieldName, boolean aStatic,  Object aNewValue) {
-		Field f;
+	public static void setFieldValue(final Object aInstance, String aFieldName, boolean aStatic,  Object aNewValue) {
+		Field f;		
+		aFieldName = getCorrectFieldName(aFieldName);
 		try {
 			if (aInstance instanceof Class && aStatic) {
 				f = getField((Class) aInstance, aFieldName);
